c8b14f452fe5fe2f80d74f30343beeb470d32304
Kpriyansh <71462800+Kpriyansh@users.noreply.github.com>
2021-09-03 02:43:09 +0530

    updating to hooks

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,54 +1,52 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import CardList from '../Components/CardList';
 import SearchBar from '../Components/Searchbar';
 import './App.css';
 import Scroll from '../Components/Scroll';
 import ErrorBoundary from '../Components/ErrorBoundary';
-class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      robots: [],
-      searchfield: '',
 
-    }
+const App = () => {
+  const [robots, setRobots] = useState([]);
+  const [searchfield, setSearchfield] = useState('');
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/users`)
+      .then(response => response.json())
+      .then((users) => {
+        setRobots(users);
+      }).catch((err) => {
+        console.log(err);
+      });
+  })
+
+  const onSearchChange = (event) => {
+    setSearchfield(event.target.value);
   }
-  componentDidMount() {
-    fetch(`https://jsonplaceholder.typicode.com/users`).then(respnse => {
-      return respnse.json();
-    })
-      .then(users => {
-        this.setState({ robots: users });
-      })
-  }
-  onSearchChange = (event) => {
-    this.setState({ searchfield: event.target.value });
-  }
-  render() {
-    //to remove this.state destructure it.
-    //const {robots,searchfield} = this.state;
-    const filterRobots = this.state.robots.filter(robots => {
-      return robots.name.toLowerCase().split(" ").join("").includes(
-        this.state.searchfield.toLowerCase().split(" ").join("")
-      );
-    })
-    if (!this.state.robots.length) {
-      return <h1 className="load-bar">LOADING...</h1>
-    } else {
-      return (
-        <React.Fragment>
-          <div className="tc">
-            <h1 className="f1">ROBOFRIENDS</h1>
-            <SearchBar searchChange={this.onSearchChange} />
-            <Scroll>
-              <ErrorBoundary>
-                <CardList robots={filterRobots} />
-              </ErrorBoundary>
-            </Scroll>
-          </div>
-        </React.Fragment>
-      )
-    }
+
+  const filterRobots = robots.filter(robot => {
+    return robot.name.toLowerCase().split(" ").join("").includes(
+      searchfield.toLowerCase().split(" ").join("")
+    )
+  })
+  if (!robots.length) {
+    return (
+      <h1 className="load-bar">LOADING...</h1>
+    )
+  } else {
+    return (
+      <React.Fragment>
+        <div className="tc">
+          <h1 className="f1">ROBOFRIENDS</h1>
+          <SearchBar searchChange={onSearchChange} />
+          <Scroll>
+            <ErrorBoundary>
+              <CardList robots={filterRobots} />
+            </ErrorBoundary>
+          </Scroll>
+        </div>
+      </React.Fragment>
+    )
   }
 }
+
 export default App;
